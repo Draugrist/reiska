1dee054feef9e5206d05a60963ac9ca1e7932eb9
Tuomas Piirainen <27916116+Draugrist@users.noreply.github.com>
2024-01-17 19:01:46 +0200

    Adds support for reflection and refraction

--- src/reiska/render.clj
+++ src/reiska/render.clj
@@ -9,6 +9,7 @@
 (def max-depth 5)
 (def bias 1e-4)
 (def default-up (v/vector 0 0 1))
+(declare trace)
 
 (defn intercept-sphere [origin ray-dir {:keys [center radius] :as sphere}]
   (let [l   (v/sub center origin)
@@ -29,7 +30,35 @@
 (defn get-closest-hit [origin ray-dir objects]
   (first (sort-by :point (filter some? (map (partial intercept-sphere origin ray-dir) objects)))))
 
-(defn object->light [hit-object point-of-hit normal-hit objects surface-color light]
+(defn fresnel [a b mix]
+  (+
+    (* b mix)
+    (* a (- 1 mix))))
+
+(defn refracted-color [{:keys [inside normal-hit ray-dir point-of-hit scene depth]}]
+  (let [ior  1.1
+        eta  (if inside ior (/ ior))
+        cosi (- (v/dot normal-hit ray-dir))
+        k    (* (- 1 eta) eta (- 1 (* cosi cosi)))
+        refracted-dir (v/normalize (v/add (v/scale ray-dir eta) (v/scale normal-hit (- (* eta cosi) (Math/sqrt k)))))]
+    (trace (v/sub point-of-hit (v/scale normal-hit bias)) refracted-dir scene (inc depth))))
+
+(defn reflected-color [{:keys [scene ray-dir sphere point-of-hit normal-hit depth] :as params}]
+  (let [facing-ratio   (- (v/dot ray-dir normal-hit))
+        fresnel-effect (fresnel (Math/pow (- 1 facing-ratio) 3) 1 0.1)
+        reflection-dir (v/normalize (v/add ray-dir (v/scale normal-hit (* 2 facing-ratio))))
+        reflection     (trace (v/add point-of-hit (v/scale normal-hit bias)) reflection-dir scene (inc depth))
+        refraction     (if
+                         (pos? (:transparency sphere))
+                         (refracted-color params)
+                         (v/vector 0 0 0))]
+    (v/mult
+      (v/add
+        (v/scale reflection fresnel-effect)
+        (v/scale refraction (* (- 1 fresnel-effect) (:transparency sphere))))
+      (:color sphere))))
+
+(defn diffuse-color [hit-object point-of-hit normal-hit objects surface-color light]
   (let [light-direction (v/normalize (v/sub (:center light) point-of-hit))
         other-objects   (remove #(= % light) objects)
         origin          (v/add point-of-hit (v/scale normal-hit bias))
@@ -43,19 +72,30 @@
                         (max 0.0
                              (v/dot normal-hit light-direction))))))))
 
+(defn surface-color [{:keys [depth sphere scene point-of-hit normal-hit] :as params}]
+  (if (and (< depth max-depth)
+           (or (pos? (:reflectivity sphere)) (pos? (:transparency sphere))))
+    (reflected-color params)
+    (reduce (partial diffuse-color sphere point-of-hit normal-hit (:objects scene)) (v/vector 0 0 0) (:lights scene))))
+
 (defn trace [origin ray-dir scene depth]
   (let [closest-hit (get-closest-hit origin ray-dir (:objects scene))]
     (if closest-hit
       (let [{:keys [point sphere]} closest-hit
-            point-of-hit (v/add origin (v/scale ray-dir point))
-            normal-hit   (v/normalize (v/sub point-of-hit (:center sphere)))
-            inside       (> (v/dot ray-dir normal-hit) 0)
-            normal-hit   (if inside (v/sub normal-hit) normal-hit)]
-        ;TODO transparent or reflective object
-        (let [surface (reduce (partial object->light sphere point-of-hit normal-hit (:objects scene)) (v/vector 0 0 0) (:lights scene))]
-          (if (:emission sphere)
-            (v/add (:emission sphere) surface)
-            surface)))
+            point-of-hit  (v/add origin (v/scale ray-dir point))
+            normal-hit    (v/normalize (v/sub point-of-hit (:center sphere)))
+            inside        (pos? (v/dot ray-dir normal-hit))
+            normal-hit    (if inside (v/sub normal-hit) normal-hit)
+            surface-color (surface-color {:scene        scene
+                                          :ray-dir      ray-dir
+                                          :sphere       sphere
+                                          :point-of-hit point-of-hit
+                                          :normal-hit   normal-hit
+                                          :depth        depth
+                                          :inside       inside})]
+        (if (:emission sphere)
+          (v/add (:emission sphere) surface-color)
+          surface-color))
       (:background scene))))
 
 (defn render [scene outfile]
